680f98efb09cfbaf4d65318d91c06952b05641d9
pinki-maker <pinkigautam2008@gmail.com>
2020-10-23 21:06:55 +0000

    Lab 4

--- main.js
+++ main.js
@@ -32,6 +32,8 @@ const IPCIDR = require('ip-cidr');
 
 const path = require('path');
 
+const { getIpv4MappedIpv6Address } = require(path.join(__dirname, 'ipv6.js'));
+
 
 
 /**
@@ -44,9 +46,33 @@ const path = require('path');
 
  */
 
-const { getIpv4MappedIpv6Address } = require(path.join(__dirname, 'ipv6.js'));
 
-/**
+
+
+
+
+
+class IpAddress {
+
+  constructor() {
+
+    // IAP's global log object is used to output errors, warnings, and other
+
+    // information to the console, IAP's log files, or a Syslog server.
+
+    // For more information, consult the Log Class guide on the Itential
+
+    // Developer Hub https://developer.itential.io/ located
+
+    // under Documentation -> Developer Guides -> Log Class Guide
+
+    log.info('Starting the IpAddress product.');
+
+  }
+
+
+
+  /**
 
  * Calculate and return the first host IP address from a CIDR subnet.
 
@@ -60,7 +86,7 @@ const { getIpv4MappedIpv6Address } = require(path.join(__dirname, 'ipv6.js'));
 
  */
 
-function getFirstIpAddress(cidrStr, callback) {
+ getFirstIpAddress(cidrStr, callback) {
 
 
 
@@ -70,7 +96,7 @@ function getFirstIpAddress(cidrStr, callback) {
 
   let callbackError = null;
 
-
+   let IPv6Address = null;
 
   // Instantiate an object from the imported class and assign the instance to variable cidr.
 
@@ -108,6 +134,8 @@ function getFirstIpAddress(cidrStr, callback) {
 
     [firstIpAddress] = cidr.toArray(options);
 
+      IPv6Address = getIpv4MappedIpv6Address(firstIpAddress);
+
   }
 
   // Call the passed callback function.
@@ -118,7 +146,11 @@ function getFirstIpAddress(cidrStr, callback) {
 
   // data as the second argument to the callback function.
 
-  return callback(firstIpAddress, callbackError);
+ return callback({ipv4: firstIpAddress, ipv6: IPv6Address}, callbackError);
+
+}
+
+
 
 }
 
@@ -126,90 +158,4 @@ function getFirstIpAddress(cidrStr, callback) {
 
 
 
-/*
-
-  This section is used to test function and log any errors.
-
-  We will make several positive and negative tests.
-
-*/
-
-function main() {
-
-  // Create some test data for getFirstIpAddress(), both valid and invalid.
-
-  let sampleCidrs = ['172.16.10.0/24', '172.16.10.0 255.255.255.0', '172.16.10.128/25', '192.168.1.216/30'];
-
-  let sampleCidrsLen = sampleCidrs.length;
-
-  // Create some test data for getIpv4MappedIpv6Address, both valid and invalid.
-
-  let sampleIpv4s = [ '172.16.10.1', '172.16.10.0/24', '172.16.10.0 255.255.255.0', '172.16.256.1', '1.1.1.-1'];
-
-  let sampleIpv4sLen = sampleIpv4s.length;
-
-
-
-  // Iterate over sampleCidrs and pass the element's value to getFirstIpAddress().
-
-  for (let i = 0; i < sampleCidrsLen; i++) {
-
-    console.log(`\n--- Test Number ${i + 1} getFirstIpAddress(${sampleCidrs[i]}) ---`);
-
-    // Call getFirstIpAddress and pass the test subnet and an anonymous callback function.
-
-    // The callback is using the fat arrow operator: () => { }
-
-    getFirstIpAddress(sampleCidrs[i], (data, error) => {
-
-      // Now we are inside the callback function.
-
-      // Display the results on the console.
-
-      if (error) {
-
-        console.error(`  Error returned from GET request: ${error}`);
-
-      }
-
-      console.log(`  Response returned from GET request: ${data}`);
-
-    });
-
-  }
-
-  // Iterate over sampleIpv4s and pass the element's value to getIpv4MappedIpv6Address().
-
-  for (let i = 0; i < sampleIpv4sLen; i++) {
-
-    console.log(`\n--- Test Number ${i + 1} getIpv4MappedIpv6Address(${sampleIpv4s[i]}) ---`);
-
-    // Assign the function results to a variable so we can check if a string or null was returned.
-
-    let mappedAddress = getIpv4MappedIpv6Address(sampleIpv4s[i]);
-
-    if( mappedAddress ) {
-
-      console.log(`  IPv4 ${sampleIpv4s[i]} mapped to IPv6 Address: ${mappedAddress}`);
-
-    } else {
-
-      console.error(`  Problem converting IPv4 ${sampleIpv4s[i]} into a mapped IPv6 address.`);
-
-    }
-
-  }
-
-}
-
-
-
-
-
-/*
-
-  Call main to run it.
-
-*/
-
-main();
+module.exports = new IpAddress;
